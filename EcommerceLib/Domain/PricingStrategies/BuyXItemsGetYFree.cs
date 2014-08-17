932f3d0cf13b9c132787f0b4dabdba3fcfbcd590
Zoltán Tóth <zshun86@gmail.com>
2014-08-18 01:56:52 +0200

    Documentation added to BuyXItemsGetYFree class.

--- EcommerceLib/Domain/PricingStrategies/BuyXItemsGetYFree.cs
+++ EcommerceLib/Domain/PricingStrategies/BuyXItemsGetYFree.cs
@@ -2,13 +2,37 @@
 
 namespace EcommerceLib.Domain.PricingStrategies
 {
+    /// <summary>
+    /// Pricing strategy for Buy-X-Items-Get-Y-Items-Free discounts.
+    /// </summary>
     public class BuyXItemsGetYFree : IPricingStrategy
     {
+        /// <summary>
+        /// The item id that the discount applies to.
+        /// </summary>
         public string ItemId { get; private set; }
+
+        /// <summary>
+        /// The minimum items needed to apply the discount.
+        /// </summary>
         public int QuantityToBuy { get; private set; }
+
+        /// <summary>
+        /// The number of items to get free when the discount applies.
+        /// </summary>
         public int QuantityToGetFree { get; private set; }
+
+        /// <summary>
+        /// The discount name.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Initializes a Buy-X-Items-Get-Y-Items-Free pricing strategy object.
+        /// </summary>
+        /// <param name="itemId">The item id that the discount applies to.</param>
+        /// <param name="quantityToBuy">The minimum items needed to apply the discount.</param>
+        /// <param name="quantityToGetFree">The number of items to get free when the discount applies.</param>
         public BuyXItemsGetYFree(string itemId, int quantityToBuy, int quantityToGetFree)
         {
             if (string.IsNullOrEmpty(itemId)) { throw new ArgumentNullException("itemId"); }
@@ -19,12 +43,23 @@ namespace EcommerceLib.Domain.PricingStrategies
             QuantityToGetFree = quantityToGetFree;
             Name = string.Format("DISCOUNT: Buy {0} get {1} free.", QuantityToBuy, QuantityToGetFree);
         }
-        
+
+        /// <summary>
+        /// Tells whether the discount applies to the supplied <see cref="OrderItem"/> or not.
+        /// </summary>
+        /// <param name="order">The <see cref="OrderItem"/> to decide the discount is appliable or not to.</param>
+        /// <returns>True if the discount applies, false otherwise.</returns>
         public bool MatchesItem(OrderItem order)
         {
             return (order.Identifier == ItemId && order.Quantity >= QuantityToBuy);
         }
 
+        /// <summary>
+        /// Calculates the discounted price for the given <see cref="OrderItem"/>.
+        /// </summary>
+        /// <param name="order">The <see cref="OrderItem"/> the price is calculated for.</param>
+        /// <returns>The discounted price.</returns>
+        /// <exception cref="ArgumentException">The discount cannot be applied for the <see cref="OrderItem"/>.</exception>
         public decimal CalculateItemPrice(OrderItem order)
         {
             if (!MatchesItem(order)) { throw new ArgumentException("The price strategy can't be applied for this order: {0}", order.ToString());}
