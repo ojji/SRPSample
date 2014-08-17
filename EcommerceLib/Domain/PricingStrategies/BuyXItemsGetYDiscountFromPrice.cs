85e43806d0a28f93de87dc6cf43fa1720a80614d
Zoltán Tóth <zshun86@gmail.com>
2014-08-18 01:36:35 +0200

    Documentation added to DefaultPricing and BuyXItemsGetYDiscountFromPrice classes.

--- EcommerceLib/Domain/PricingStrategies/BuyXItemsGetYDiscountFromPrice.cs
+++ EcommerceLib/Domain/PricingStrategies/BuyXItemsGetYDiscountFromPrice.cs
@@ -2,13 +2,37 @@
 
 namespace EcommerceLib.Domain.PricingStrategies
 {
+    /// <summary>
+    /// Pricing strategy for Buy-X-Items-Get-Y-Percent-Off discounts.
+    /// </summary>
     public class BuyXItemsGetYDiscountFromPrice : IPricingStrategy
     {
+        /// <summary>
+        /// The item id that the discount applies to.
+        /// </summary>
         public string ItemId { get; private set; }
+
+        /// <summary>
+        /// The minimum items needed to apply the discount.
+        /// </summary>
         public int DiscountedQuantity { get; private set; }
+
+        /// <summary>
+        /// The discount amount between 0.0m and 1.0m.
+        /// </summary>
         public decimal Discount { get; private set; }
+
+        /// <summary>
+        /// The discount name.
+        /// </summary>
         public string Name { get; private set; }
 
+        /// <summary>
+        /// Initializes a Buy-X-Item-Get-Y-Percent-Off pricing strategy object.
+        /// </summary>
+        /// <param name="itemId">The item id that the discount applies to.</param>
+        /// <param name="discountedQuantity">The minimum items needed to apply the discount.</param>
+        /// <param name="discount">The discount amount between 0.0m and 1.0m.</param>
         public BuyXItemsGetYDiscountFromPrice(string itemId, int discountedQuantity, decimal discount)
         {
             if (string.IsNullOrEmpty(itemId)) { throw new ArgumentNullException("itemId"); }
@@ -20,11 +44,22 @@ namespace EcommerceLib.Domain.PricingStrategies
             Name = string.Format("DISCOUNT: Buy {0}, get {1:P} off.", DiscountedQuantity, Discount);
         }
         
+        /// <summary>
+        /// Tells whether the discount applies to the supplied <see cref="OrderItem"/> or not.
+        /// </summary>
+        /// <param name="order">The <see cref="OrderItem"/> to decide the discount is appliable or not to.</param>
+        /// <returns>True if the discount applies, false otherwise.</returns>
         public bool MatchesItem(OrderItem order)
         {
             return (order.Identifier == ItemId && order.Quantity >= DiscountedQuantity);
         }
 
+        /// <summary>
+        /// Calculates the discounted price for the given <see cref="OrderItem"/>.
+        /// </summary>
+        /// <param name="order">The <see cref="OrderItem"/> the price is calculated for.</param>
+        /// <returns>The discounted price.</returns>
+        /// <exception cref="ArgumentException">The discount cannot be applied for the <see cref="OrderItem"/>.</exception>
         public decimal CalculateItemPrice(OrderItem order)
         {
             if (!MatchesItem(order)) { throw new ArgumentException("The price strategy can't be applied for this order: {0}", order.ToString());}
--- EcommerceLib/Domain/PricingStrategies/DefaultPricing.cs
+++ EcommerceLib/Domain/PricingStrategies/DefaultPricing.cs
@@ -1,14 +1,31 @@
 ﻿namespace EcommerceLib.Domain.PricingStrategies
 {
+    /// <summary>
+    /// The default, full-price pricing strategy.
+    /// </summary>
     public class DefaultPricing : IPricingStrategy
     {
+        /// <summary>
+        /// The pricing strategy name.
+        /// </summary>
         public string Name { get { return "Full price."; } }
 
+        /// <summary>
+        /// Tells whether the pricing strategy applies to the given <see cref="OrderItem"/>.
+        /// It returns always true.
+        /// </summary>
+        /// <param name="order">The <see cref="OrderItem"/> to decide the discount is appliable or not to.</param>
+        /// <returns>Always true.</returns>
         public bool MatchesItem(OrderItem order)
         {
             return true;
         }
 
+        /// <summary>
+        /// Calculates the full price for the given <see cref="OrderItem"/>.
+        /// </summary>
+        /// <param name="order">The <see cref="OrderItem"/> the price is calculated for.</param>
+        /// <returns>The full price.</returns>
         public decimal CalculateItemPrice(OrderItem order)
         {
             return order.ItemCost*order.Quantity;
